9cad76804942190915dcda371250adc867bf6009
Desai Hardik <87645745+imhardikdesai@users.noreply.github.com>
2022-11-03 10:39:12 +0530

    Working React Form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,17 +2,53 @@ import React, { useState } from "react"
 import "./Contact.css"
 
 export default function Contact() {
+    URL = process.env.FIREBASE_DATABASE_URL;
+    console.log(URL);
     const [data, setData] = useState({
         name: "",
         email: "",
         message: "",
+        address: "",
         phone: "",
     })
+
+    // Iterable JSON 
+
+    const { name, email, message, address, phone } = data;
+
     const setContactData = (e) => {
-
+        let name = e.target.name;
+        let value = e.target.value;
+        setData({ ...data, [name]: [value] }) // we can send data in state without delete old data
     }
-    const sendData = (e) => {
 
+    const sendData = async (e) => {
+        e.preventDefault(); // Use for stop to getting Page "Unable to POST/"
+        if (name && email && message && address && phone) {
+            let res = await fetch('https://contact-form-react-2a6a6-default-rtdb.firebaseio.com/userData.json', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ name, email, message, address, phone })
+                // Web can directly send JSON
+                // body: JSON.stringify(data)
+            });
+            // For set empty field
+            if (res) {
+                setData({
+                    name: "",
+                    email: "",
+                    message: "",
+                    address: "",
+                    phone: "",
+                });
+                alert("Message sent Successfully")
+            }
+        } else {
+            alert("Fill all the Field");
+        }
     }
     return (
         <>
@@ -23,23 +59,23 @@ export default function Contact() {
                         <form method="post">
                             <div className="formBox">
                                 <div className="inputBox w50">
-                                    <input type="text" name="name" onChange={setContactData} required />
+                                    <input type="text" name="name" value={data.name} onChange={setContactData} required />
                                     <span>Name</span>
                                 </div>
                                 <div className="inputBox w50">
-                                    <input type="text" name="Address" onChange={setContactData} required />
+                                    <input type="text" name="address" value={data.address} onChange={setContactData} required />
                                     <span>Address</span>
                                 </div>
                                 <div className="inputBox w50">
-                                    <input type="email" name="email" onChange={setContactData} required />
+                                    <input type="email" name="email" value={data.email} onChange={setContactData} required />
                                     <span>Email Address</span>
                                 </div>
                                 <div className="inputBox w50">
-                                    <input type="text" name="number" onChange={setContactData} required />
+                                    <input type="text" name="phone" value={data.phone} onChange={setContactData} required />
                                     <span>Mobile Number</span>
                                 </div>
                                 <div className="inputBox w100">
-                                    <textarea required name="message" onChange={setContactData}></textarea>
+                                    <textarea required name="message" value={data.message} onChange={setContactData}></textarea>
                                     <span>Write your message here...</span>
                                 </div>
                                 <div className="inputBox w100">
