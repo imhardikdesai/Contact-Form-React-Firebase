4dde14c598452ae1966a1671907b072ead234aa4
Desai Hardik <87645745+imhardikdesai@users.noreply.github.com>
2022-12-30 21:37:42 +0530

    edit links in env.local

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import Contact from "./components/Contact";
 
 function App() {
-  const URL = process.env.REACT_APP_FIREBASE_DATABASE_URL;
+  // const URL = process.env.REACT_APP_FIREBASE_DATABASE_URL;
+  const URL = 'https://contact-form-react-2a6a6-default-rtdb.firebaseio.com/userData.json';
   return (
     <div className="App">
       <Contact url={URL} />
